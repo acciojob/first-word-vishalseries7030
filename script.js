6f4dc6df7790566d7328f5474b549eb72b95c854
vishal payyawar <139535850+vishalseries7030@users.noreply.github.com>
2025-08-08 11:20:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
-  // your code here
+  s = s.trim();
+  let spaceIndex = s.indexOf(" ");
+  if (spaceIndex === -1) {
+    return s;
+  }
+  return s.substring(0, spaceIndex);
 }
-
-// Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
+
